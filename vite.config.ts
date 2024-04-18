214247b9ac8d450b410c99e9aa14795976387fac
顾孝标 <guxiaobiao@xinxuan.net>
2024-04-18 15:13:51 +0800

    feat: change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    extensions: ['.vue', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  plugins: [
+    react(),
+  ],
   css: {
     preprocessorOptions: {
       less: {
