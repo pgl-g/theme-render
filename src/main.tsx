b2395c18bb11267a5aa558251d5bb646dd7afc0d
顾孝标 <guxiaobiao@xinxuan.net>
2024-04-18 14:32:39 +0800

    feat: init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import zhCN from '@xlion/component/dist/locale/zh_CN';
+import { ConfigProvider } from '@xlion/component';
+import '@xlion/component/dist/index.less';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import './index.less'
+
+
+dayjs.locale('zh-cn');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>,
+  </ConfigProvider>,
 )
