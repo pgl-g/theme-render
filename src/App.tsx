e94bb6d0143c96e5f67fadfad8804e3d8355e85d
顾孝标 <guxiaobiao@xinxuan.net>
2024-04-20 17:28:59 +0800

    fix: change

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,44 @@
 
 
 import './App.less'
+
+import { Tabs } from '@xlion/component';
+
 import GlobalStyle from './pages/GlobalStyle';
 
 
 
 function App() {
 
+
+  const onChange = (val) => {
+    console.log(val, '---val')
+  }
+
   return (
-    <GlobalStyle />
+    <Tabs
+      defaultActiveKey="1"
+      onChange={onChange}
+      style={{ marginLeft: 44 }}
+      items={[
+        {
+          label: `全局样式定制`,
+          key: '1',
+          children: <GlobalStyle />,
+        },
+        {
+          label: `组件样式定制`,
+          key: '2',
+          children: `Content of Tab Pane 2`,
+        },
+        {
+          label: `预览主题`,
+          key: '3',
+          children: `Content of Tab Pane 3`,
+        },
+      ]}
+    />
+
   )
 }
 
